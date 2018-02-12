9fe164f4a886aa76c572711cd5faccfaf4c41d82
ChickenPaella <lhbv1421@hanmail.net>
2018-02-12 18:34:59 +0900

    빌드 테스트

--- src/main/java/com/aiolos/sloot/HomeController.java
+++ src/main/java/com/aiolos/sloot/HomeController.java
@@ -11,17 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces="application/text; charset=utf8")
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
